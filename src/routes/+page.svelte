e51341c28d1d5f55b9682c4b89553999e6280c95
Keanu Tuxford (1000064407) <TUXFKH1@student.op.ac.nz>
2023-07-28 09:49:20 +1200

    made piggy page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,32 @@
 <nav>
-	<a href="/">home</a>
-	<a href="/about">about</a>
+	<a href="/">HOME</a>
+	<a href="/about">ABOUT</a>
 </nav>
 
-<h1>home</h1>
-<p>this is the home page.</p>
+<svg viewBox="0 0 960 300">
+  <symbol id="s-text">
+    <text text-anchor="middle" x="50%" y="80%">Pigs</text>
+    
+    
+  </symbol>
+
+  <g class = "g-ants">
+    <use xlink:href="#s-text" class="text-copy"></use>
+    <use xlink:href="#s-text" class="text-copy"></use>
+    <use xlink:href="#s-text" class="text-copy"></use>
+    <use xlink:href="#s-text" class="text-copy"></use>
+    <use xlink:href="#s-text" class="text-copy"></use>
+  </g>
+</svg>
+  <img src="https://cdn.wallpapersafari.com/72/98/vY9A7Z.jpg" alt="doge" style="width:900px;height:600px;">
+ 
+  <svelte:head>
+
+    <link rel="stylesheet" href="css/main.css" />
+
+  </svelte:head>
+
+
+<div></div>
+
+  <a target="_blank" href="https://www.youtube.com/">YouTube</a>

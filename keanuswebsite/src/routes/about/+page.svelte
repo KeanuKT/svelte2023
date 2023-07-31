763f741f1a3f121e5a490a85eafe183b0aff689f
Keanu Tuxford (1000064407) <TUXFKH1@student.op.ac.nz>
2023-08-01 11:02:44 +1200

    created root

---
+++ keanuswebsite/src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
+<h1>about</h1>
+<p>this is the about page.</p>
+<img src="https://www.allrecipes.com/thmb/ZyYXwx4FKm3Y13OJGpUjVwx8Ftg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/3882376-bacon-for-the-family-or-a-crowd-sanzoe-4x3-1-8711ddbb6af242f184a79e5e72efc7e3.jpg" alt="doge" style="width:900px;height:600px;">
